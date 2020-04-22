b22a32c3ba309d697501e2615bed137f5f82c3fa
Kamrul_Hasan <48267671+Sara-Hasan@users.noreply.github.com>
2020-04-22 17:22:36 +0600

    Add files via upload

---
+++ MISERMAN - Wise And Miser.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define  ll             long long int
+
+ll a, b, c,dp[103][103],vis[103][103],ara[103][103],k;
+ll fun( ll x, ll y )
+{
+    if(x<a && x>=0 && y <b && y>=0 )
+    {
+        ll &ret1= dp[x][y];
+        ll &v=vis[x][y];
+        if(v==k) return ret1 ;
+        v=k;
+        ret1 = ara[x][y]+fun(x+1,y);
+        ret1 = min(ret1, ara[x][y]+fun(x+1,y+1));
+        ret1 = min(ret1, ara[x][y]+fun(x+1,y-1));
+        if (x==0) ret1 = min(ret1, fun(x,y+1));
+        return ret1 ;
+    }
+    else
+    {
+        if(x==a)return 0 ;
+        return INT_MAX;
+    }
+}
+int main()
+{
+    ll  d,e,f,i,j,cnt1=0,cnt2=0,sum1=0,sum2=0,chk1=0,chk2=0,T;
+    k++;
+    scanf("%lld%lld",&a,&b);
+    for( i = 0 ; i < a ; i++)
+        for( j = 0 ; j < b ; j++ )
+            scanf("%lld",&ara[i][j]);
+    printf("%lld\n",fun(0,0));
+    return 0 ;
+}
+//https://www.spoj.com/problems/MISERMAN/
