67a5e46586a92f0570425b6f1abd539879e63236
Kamrul_Hasan <48267671+Sara-Hasan@users.noreply.github.com>
2020-07-15 00:29:07 +0600

    Add files via upload

---
+++ LPS - Longest Palindromic Substring.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string text;
+int min(int a, int b)
+{
+	int res = a;
+	if(b < a)
+		res = b;
+	return res;
+}
+
+void findLongestPalindromicString()
+{
+	int N = text.size();
+	if(N == 0)  return;
+	N = 2*N + 1;
+	int L[N];
+	L[0] = L[1]=0;
+	int C=1,R=2,i=0,iMirror,mx=0,center=0,start=-1,endd=-1,diff=-1;
+	for (i = 2; i < N; i++)
+	{
+		iMirror = 2*C-i;
+		L[i] = 0;
+		diff = R - i;
+		if(diff > 0) L[i] = min(L[iMirror], diff);
+
+		while(((i+L[i])<N &&(i-L[i])>0)&&(((i+L[i]+1)%2==0)||(text[(i+L[i]+1)/2]==text[(i-L[i]-1)/2]))) L[i]++;
+
+		if(L[i]>mx)
+		{
+			mx = L[i];
+			center = i;
+		}
+		if (i+L[i]>R)
+		{
+			C = i;
+			R = i+L[i];
+		}
+	}
+	printf("%d\n",mx);
+}
+
+int main(int argc, char *argv[])
+{
+    int a;
+	cin >> a >>text ;
+	findLongestPalindromicString();
+	return 0;
+}
+//https://www.spoj.com/problems/LPS/
